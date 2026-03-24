47c36c11a972f5a302fd04383f2ed1e4f5e42781
Olivier Bélanger <belangerolivier@outlook.com>
2026-03-23 23:19:18 -0400

    feat: add ODataResponse wrapper for SSRS REST list endpoints

---
+++ src/SsrsClient/Rest/Models/ODataResponse.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace SsrsClient.Rest.Models
+{
+    /// <summary>OData-style wrapper returned by SSRS REST list endpoints.</summary>
+    internal sealed class ODataResponse<T>
+    {
+        [JsonPropertyName("value")]
+        public List<T> Value { get; private set; } = new List<T>();
+    }
+}
