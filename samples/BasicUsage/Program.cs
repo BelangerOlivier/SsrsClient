41ce496da34dfdf98d3b51e0a44773a1b542c420
Olivier Bélanger <belangerolivier@outlook.com>
2026-04-14 11:06:29 -0400

    feat: add basic program.cs to basic usage project

---
+++ samples/BasicUsage/Program.cs
@@ -0,0 +1,9 @@
+﻿using SsrsClient;
+
+var client = new SsrsClientBuilder()
+    .WithBaseUrl("")
+    .WithNtlm("", "", "")
+    .Build();
+
+var report = await client.GetReportAsync("");
+Console.WriteLine(report.Id);
